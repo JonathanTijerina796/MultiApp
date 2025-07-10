78670b637bd69c2a8281ee705c9fffeba43c3832
jonathan tijerina <jon.music796@gmail.com>
2025-07-10 11:22:01 -0600

    Initial Commit

---
+++ CursoiOS2/CursoiOS2App.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOS2App.swift
+//  CursoiOS2
+//
+//  Created by Jona on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOS2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CursoiOS2Tests/CursoiOS2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOS2Tests.swift
+//  CursoiOS2Tests
+//
+//  Created by Jona on 10/07/25.
+//
+
+import Testing
+@testable import CursoiOS2
+
+struct CursoiOS2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
